f3382919164db783cbc45bab99c77e46de61ebea
Jose Quintero <tomark4@gmail.com>
2023-06-07 16:58:31 -0400

    fix: Change content in index page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -9,15 +9,6 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-          </a>
-        </div>
       </div>
 
       <div className={styles.center}>
