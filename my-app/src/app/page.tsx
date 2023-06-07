40b224b5898f6a21b532ab1ac00ac6b9559ee109
Jose Quintero <tomark4@gmail.com>
2023-06-07 16:37:37 -0400

    style: Remove element on index page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,15 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            By{" "}
           </a>
         </div>
       </div>
@@ -91,5 +83,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
